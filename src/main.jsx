6f852b3a656fcf6d3aac8401ce0c108de5c724fc
Pranav Jayasankar <pranavjayasankar@Pranavs-MacBook-Air-2.local>
2025-04-06 22:18:36 +0530

    Update: final working version with API + UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import WizardPalChat from './components/ui/WizardPalChat';
-import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
